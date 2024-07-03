21ed39bd1af39a415fff13e40c7d60f712315d64
david-gelu <david.gelu90@gmail.com>
2024-07-03 13:01:03 +0300

    fix layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,14 @@ import AddToDo from "@/components/shared/AddToDo"
 import styles from "./page.module.css"
 import { ToDo } from "@/components/shared/ToDo"
 import { todoType } from "@/types/todoTypes"
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef, useLayoutEffect } from "react"
 
 const Home = () => {
   const [dataUpdated, setDataUpdated] = useState<todoType[]>([])
   const [refresh, setRefresh] = useState(false)
   const [search, setSearch] = useState('')
+
+  const elementRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,12 +28,22 @@ const Home = () => {
     fetchData()
   }, [refresh, search])
 
+  useEffect(() => {
+    if (elementRef.current) {
+      const elem = document.querySelector(':root') as HTMLElement
+      elem.style.setProperty('--height', `${elementRef.current.clientHeight}px`)
+    }
+  }, []);
+
+
   return (
     <main className={styles.main}>
-      <div style={{ marginBottom: '1.5rem', width: '100%', display: 'flex', flexWrap: 'wrap', gap: '1rem', alignItems: 'center', justifyContent: 'space-between' }}>
-        <h1>Add todo app</h1> <input className='input' onChange={(e) => setSearch(e.target.value)} placeholder="Search todos by name or price" style={{ minWidth: '16rem', maxWidth: '20rem', margin: '0.5rem 0 0.5rem auto' }} />
+      <div ref={elementRef}>
+        <div style={{ marginBottom: '1.5rem', width: '100%', display: 'flex', flexWrap: 'wrap', gap: '1rem', alignItems: 'center', justifyContent: 'space-between' }}>
+          <h1>Add todo app</h1> <input className='input' onChange={(e) => setSearch(e.target.value)} placeholder="Search todos by name or price" style={{ minWidth: '16rem', maxWidth: '20rem', margin: '0.5rem 0 0.5rem auto' }} />
+        </div>
+        <AddToDo setRefresh={setRefresh} />
       </div>
-      <AddToDo setRefresh={setRefresh} />
       <div style={{ width: '100%' }} className="todo-container">
         {dataUpdated.map((todo) => (
           <ToDo setRefresh={setRefresh} todo={todo} key={todo.id} />
